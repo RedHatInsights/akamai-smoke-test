20c2ca6c5e39ccc53706031c8390b2634ce0db94
Ian Page Hands <iphands@gmail.com>
2019-04-09 10:07:49 -0400

    Fix polluting the original create_conection by doing it only once

--- test/decorators.py
+++ test/decorators.py
@@ -1,8 +1,8 @@
 from urllib3.util import connection
+_orig_create_connection = connection.create_connection
 def modify_ip(ip):
     def decorator(func):
         def wrapper(data_element):
-            _orig_create_connection = connection.create_connection
             def my_create_connection(address, *args, **kwargs):
                 host, port = address
                 return _orig_create_connection((ip, port), *args, **kwargs)
