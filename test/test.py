1f86f1eab9a8fa2c9558290b86ffbcebda298a79
Ian Page Hands <iphands@gmail.com>
2019-04-09 10:08:10 -0400

    add check for UHC on cloud without cookie test

--- test/test.py
+++ test/test.py
@@ -13,7 +13,7 @@ cookies = { 'x-rh-insights-alpha': 'flipmodesquadisthegreatest' }
 
 output_data = {}
 
-def getUrl(path, release):
+def getUrl(path, release='stable'):
     if release == 'beta':
         return baseurl + '/beta' + path
     return baseurl + path
@@ -31,34 +31,55 @@ def do_urls(env, data_element, release = 'stable'):
                     expected = '/apps/' + data_key
                     assert expected in src, "unexpected app id at {}\n{}\n{}".format(url, r.text, r.headers)
 
+                    print(r)
                     if env == 'stage':
-                        assert 'X-Akamai-Staging' in r.headers, 'expected to see staging header in {}'.format(r.headers)
+                        assert 'X-Akamai-Staging' in r.headers, 'expected to see staging header in {}\n{}'.format(r.headers, url)
                     else:
-                        assert 'X-Akamai-Staging' not in r.headers, 'expected to not see staging header in {}'.format(r.headers)
+                        assert 'X-Akamai-Staging' not in r.headers, 'expected to not see staging header in {}\n{}'.format(r.headers, url)
 
                     if path not in output_data:
                         output_data[path] = DotDict({ 'url': url })
                     output_data[path][env + '_hash'] = hashlib.md5(r.text.encode('utf-8')).hexdigest()
 
 DATA = Utils.getData()
+PROD_IP  = '104.112.254.145'
+STAGE_IP = '23.201.3.166'
 
+UHC_ON_CLOUD_URLS = [ getUrl('/'), getUrl('/clusters/') ]
+
+@pytest.mark.parametrize('data_element', UHC_ON_CLOUD_URLS)
+@modify_ip(PROD_IP)
+def test_uhc_unchromed_still_works_prod(data_element):
+    r = requests.get(data_element)
+    assert '<title>OpenShift Unified Hybrid Cloud</title>' in r.text
+    assert ' src="/clusters/bundle.main.js' in r.text
+
+@pytest.mark.parametrize('data_element', UHC_ON_CLOUD_URLS)
+@modify_ip(STAGE_IP)
+def test_uhc_unchromed_still_works_stage(data_element):
+    r = requests.get(data_element)
+    assert '<title>OpenShift Unified Hybrid Cloud</title>' in r.text
+    assert ' src="/clusters/bundle.main.js' in r.text
+
+# PROD
 @pytest.mark.parametrize('data_element', DATA.items(), ids=list(DATA.keys()))
-@modify_ip('104.112.254.145')
+@modify_ip(PROD_IP)
 def test_urls_prod_stable(data_element):
     do_urls('prod', data_element, release = 'beta')
 
 @pytest.mark.parametrize('data_element', DATA.items(), ids=list((s + '-beta' for s in DATA.keys())))
-@modify_ip('104.112.254.145')
+@modify_ip(PROD_IP)
 def test_urls_prod_beta(data_element):
     do_urls('prod', data_element, release = 'beta')
 
+# STAGE
 @pytest.mark.parametrize('data_element', DATA.items(), ids=list(DATA.keys()))
-@modify_ip('23.201.3.166')
+@modify_ip(STAGE_IP)
 def test_urls_stage_stable(data_element):
     do_urls('stage', data_element)
 
 @pytest.mark.parametrize('data_element', DATA.items(), ids=list((s + '-beta' for s in DATA.keys())))
-@modify_ip('23.201.3.166')
+@modify_ip(STAGE_IP)
 def test_urls_stage_beta(data_element):
     do_urls('stage', data_element, release = 'beta')
 
